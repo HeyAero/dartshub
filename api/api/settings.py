775851e24d710d675758037f485226e434aa283f
HeyAero <aeroyts@icloud.com>
2021-05-14 14:29:39 +0100

    add dependencies needed for postgres to run on django

--- api/api/settings.py
+++ api/api/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'api.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'dartshub',
+        'USER': 'admin',
+        'PASSWORD': 'pass',
+        'HOST': 'db',
+        'PORT': '5432',
     }
 }
 
