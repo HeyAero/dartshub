f9c6227e8071fbba7c7bd24d44a221e28f3a42e7
HeyAero <aeroyts@icloud.com>
2021-05-16 21:25:08 +0100

    add POST request handler for adding new users

--- api/users/views.py
+++ api/users/views.py
@@ -15,10 +15,12 @@ from rest_framework.decorators import api_view
 def index(request):
   if request.method == 'GET':
     users = Users.objects.all()
-
-    title = request.GET.get('title', None)
-    if title is not None:
-      users = users.filter(title__icontains=title)
-    
     users_serializer = UsersSerializers(users, many=True)
     return JsonResponse(users_serializer.data, safe=False)
+  elif request.methid == 'POST':
+    users_data = JSONParser().parse(request)
+    users_serializer = UsersSerializers(data=users_data)
+    if users_serializer.is_valid():
+      users_serializer.save()
+      return JsonResponse(users_serializer.data, status=status.HTTP_201_CREATED)
+    return JsonResponse(users_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
