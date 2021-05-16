44855b4fd5f0a890b7742649cc2311d97f004a26
HeyAero <aeroyts@icloud.com>
2021-05-16 21:06:50 +0100

    add users serializer

--- server/api/api/settings.py
+++ server/api/api/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = 'django-insecure-$&(ztympm+wp85zhv2kr!1fqb=*-uriv6^hvyjd6v-hk%#ft!r
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
+CORS_ORIGIN_ALLOW_ALL = True
 ALLOWED_HOSTS = []
 
 
