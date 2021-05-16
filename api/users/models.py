fcff21365c2f83dc5c6c7ef5ed8e0d9d28d250b1
HeyAero <aeroyts@icloud.com>
2021-05-16 20:03:27 +0100

    update users model to correctly format user and update settings to use new apps

--- api/users/models.py
+++ api/users/models.py
@@ -2,4 +2,8 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
-user = User.objects.create_user()
+class Users(models.Model):
+  profile = models.OneToOneField(User, on_delete=models.SET_NULL, null=True)
+
+  def __str__(self):
+    return f'{self.profile.username}'
