255b5eee17b5e8540179c24c45fe6809908f1019
HeyAero <aeroyts@icloud.com>
2021-05-17 09:22:52 +0100

    fix issue where views for stats was in urls py, add stats steralizer and add GET request for stats index

--- server/api/stats/urls.py
+++ server/api/stats/urls.py
@@ -1,16 +1,6 @@
 from django.urls import path
-from stats import views
-from django.core.serializers import serialize
+from . import views
 
-from django.http.response import JsonResponse
-from rest_framework.parsers import JSONParser 
-from rest_framework import status
-
-from django.contrib.auth.models import User
-from users.serializers import UsersSerializers
-from rest_framework.decorators import api_view
-
-# Create your views here.
-@api_view(['GET'])
-def index(request):
-  return False
+urlpatterns = [
+  path('', views.index, name='stats-index'),
+]
