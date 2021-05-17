edfa078ef6e0ee3b181a955b2d591ff951868d9c
HeyAero <aeroyts@icloud.com>
2021-05-17 12:14:28 +0100

    add attempt at user token authentication, will be continued later

--- server/api/api/urls.py
+++ server/api/api/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+from users.models import CustomAuthToken
+from rest_framework.authtoken import views
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('users/', include('users.urls')),
     path('stats/', include('stats.urls')),
+    path('token/', views.obtain_auth_token),
 ]
--- server/api/users/views.py
+++ server/api/users/views.py
@@ -9,6 +9,9 @@ from rest_framework import status
 from django.contrib.auth.models import User
 from users.serializers import UsersSerializers
 from rest_framework.decorators import api_view
+from rest_framework.authtoken.models import Token
+from rest_framework.response import Response
+
 
 # Create your views here.
 @api_view(['GET', 'POST'])
@@ -24,3 +27,9 @@ def index(request):
       users_serializer.save()
       return JsonResponse(users_serializer.data, status=status.HTTP_201_CREATED)
     return JsonResponse(users_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
+@api_view(['GET'])
+def get_tokens(request):
+  if request.method == 'GET':
+    tokens = Token.objects.all()
+    print(tokens)
