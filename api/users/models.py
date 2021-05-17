58f9350c8337453efcba3be4d66cc0ff68de5e64
HeyAero <aeroyts@icloud.com>
2021-05-17 22:23:34 +0100

    update whole app with refactore and restart

--- api/users/models.py
+++ api/users/models.py
@@ -1,32 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-from rest_framework.authtoken.views import ObtainAuthToken
-from rest_framework.authtoken.models import Token
-from rest_framework.response import Response
-
-
-
-from django.conf import settings
-from django.db.models.signals import post_save
-from django.dispatch import receiver
-from rest_framework.authtoken.models import Token
-
-
-# Create your models here.
-class CustomAuthToken(ObtainAuthToken):
-
-    def post(self, request, *args, **kwargs):
-        serializer = self.serializer_class(data=request.data, context={'request': request})
-        serializer.is_valid(raise_exception=True)
-        user = serializer.validated_data['user']
-        token, created = Token.objects.get_or_create(user=user)
-        return Response({
-            'token': token.key,
-            'id': user.pk
-        })
-
-@receiver(post_save, sender=settings.AUTH_USER_MODEL)
-def create_auth_token(sender, instance=None, created=False, **kwargs):
-  if created:
-    print("Test")
-    Token.objects.create(user=instance)
+class CustomUser(AbstractUser):
+  dummy_data = models.CharField(blank=True, max_length=120)
--- api/users/urls.py
+++ api/users/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from users import views
 
 urlpatterns = [
-  path('', views.index, name='users-index'),
-  path('tokens', views.get_tokens, name="users-tokens")
+  # path('', views.index, name='users-index'),
+  # path('tokens', views.get_tokens, name="users-tokens"),
+  # path('login', views.login_user, name="users-login"),
 ]
