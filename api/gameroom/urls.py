7a5bfbbbcbb37fb5c9753d02a9e9bfcbc1379135
mfy <Faisaly1234@outlook.com>
2021-05-17 16:10:29 +0100

    added check to groups to limit users in room

--- api/gameroom/urls.py
+++ api/gameroom/urls.py
@@ -2,5 +2,6 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('<str:room_name>/', views.room, name ='room'),
+    path('', views.lobby, name ='lobby'),
+    path('room/<str:room_name>/', views.room, name ='room'),
 ]
