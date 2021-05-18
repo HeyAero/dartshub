e98ac71afc1141d917e08dd541b1ab018e0e3256
HeyAero <aeroyts@icloud.com>
2021-05-18 21:45:51 +0100

    add coverage to dependencies and update docker to include it and add base tests for stats

--- _scripts/startTest.sh
+++ _scripts/startTest.sh
@@ -1,2 +1,1 @@
-docker-compose -f docker-compose.yaml -f docker-compose.test.yaml up -d
-docker exec -it test_api bash -c "npm install && npm test"
+docker-compose -f docker-compose.yaml -f docker-compose.test.yaml up
