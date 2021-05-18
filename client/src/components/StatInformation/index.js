8032edb00291b67f410d2c89bf2a5d0474bd5b2c
HeyAero <aeroyts@icloud.com>
2021-05-18 14:38:06 +0100

    add getStatsInformation function to user page which fetches user stats data and displays it on their personalised page

--- client/src/components/StatInformation/index.js
+++ client/src/components/StatInformation/index.js
@@ -1,6 +1,8 @@
 import React from "react";
+import { getAuthInstance } from '../../actions'
 
-const StatInformation = () => {
+const StatInformation = ({totalGames, wins, loses, highestFinish, doublesHit, hit180, hit160, hit140, hit120, hit100, hit80, hit60, hit0}) => {
+
     return (
         <>
         <h1 id="uname-title">Look At How Well You've Been Doing username</h1>
@@ -13,15 +15,15 @@ const StatInformation = () => {
 
                             <tr>
                                 <td className="headers">Games Played</td>
-                                <td>rrrrrrrrrrrr</td>
+                                <td>{totalGames}</td>
                             </tr>
                             <tr>
                                 <td className="headers">Wins</td>
-                                <td>rrrrrrrrrrrr</td>
+                                <td>{wins}</td>
                             </tr>
                             <tr>
                                 <td className="headers">Losses</td>
-                                <td>rrrrrrrrrrrr</td>
+                                <td>{loses}</td>
                             </tr>
 
                         </tbody>
@@ -35,11 +37,11 @@ const StatInformation = () => {
 
                             <tr>
                                 <td className="headers">Highest Finish</td>
-                                <td>rrrrrrrrrrrr</td>
+                                <td>{highestFinish}</td>
                             </tr>
                             <tr>
                                 <td className="headers">Doubles Hit</td>
-                                <td>rrrrrrrrrrrr</td>
+                                <td>{doublesHit}</td>
                             </tr>
 
                         </tbody>
@@ -51,35 +53,35 @@ const StatInformation = () => {
                         <tbody>
                             <tr>
                                 <td className="headers">180s</td>
-                                <td>rrrrrrrrrrrr</td>
+                                <td>{hit180}</td>
                             </tr>
                             <tr>
                                 <td className="headers">160-179</td>
-                                <td>rrrrrrrrrrrr</td>
+                                <td>{hit160}</td>
                             </tr>
                             <tr>
                                 <td className="headers">140-159</td>
-                                <td>rrrrrrrrrrrr</td>
+                                <td>{hit140}</td>
                             </tr>
                             <tr>
                                 <td className="headers">120-139</td>
-                                <td>rrrrrrrrrrrr</td>
+                                <td>{hit120}</td>
                             </tr>
                             <tr>
                                 <td className="headers">100-119</td>
-                                <td>rrrrrrrrrrrr</td>
+                                <td>{hit100}</td>
                             </tr>
                             <tr>
                                 <td className="headers">80-99</td>
-                                <td>rrrrrrrrrrrr</td>
+                                <td>{hit80}</td>
                             </tr>
                             <tr>
                                 <td className="headers">60-79</td>
-                                <td>rrrrrrrrrrrr</td>
+                                <td>{hit60}</td>
                             </tr>
                             <tr>
                                 <td className="headers">0-59</td>
-                                <td>rrrrrrrrrrrr</td>
+                                <td>{hit0}</td>
                             </tr>
                         </tbody>
                     </table>
--- client/src/pages/User/index.js
+++ client/src/pages/User/index.js
@@ -6,6 +6,44 @@ const User = () => {
     const [auth, setAuth] = React.useState(false);
     const [username, setUsername] = React.useState(""); 
     const [email, setEmail] = React.useState(""); 
+    const [threeDartAvg, setThreeDartAvg] = React.useState(""); 
+    const [oneDartAvg, setOneDartAvg] = React.useState(""); 
+    const [wins, setWins] = React.useState(""); 
+    const [loses, setLoses] = React.useState(""); 
+    const [totalGames, setTotalGames] = React.useState(""); 
+    const [highestFinish, setHighestFinish] = React.useState(""); 
+    const [doublesHit, setDoublesHit] = React.useState(""); 
+    const [hit180, setHit180] = React.useState(""); 
+    const [hit160, setHit160] = React.useState(""); 
+    const [hit140, setHit140] = React.useState(""); 
+    const [hit120, setHit120] = React.useState(""); 
+    const [hit100, setHit100] = React.useState(""); 
+    const [hit80, setHit80] = React.useState(""); 
+    const [hit60, setHit60] = React.useState(""); 
+    const [hit0, setHit0] = React.useState(""); 
+
+    async function getStatsInformation() {
+        try {
+            const response = await getAuthInstance.get('/stats/user/')
+            setThreeDartAvg(response.data.three_dart_avg);
+            setOneDartAvg(response.data.one_dart_avg);
+            setWins(response.data.wins);
+            setLoses(response.data.loses);
+            setTotalGames(response.data.total_games);
+            setHighestFinish(response.data.highest_finish);
+            setDoublesHit(response.data.doubles_hit);
+            setHit180(response.data.hit_180);
+            setHit160(response.data.hit_160_179);
+            setHit140(response.data.hit_140_159);
+            setHit120(response.data.hit_120_139);
+            setHit100(response.data.hit_100_119);
+            setHit80(response.data.hit_80_99);
+            setHit60(response.data.hit_60_79);
+            setHit0(response.data.hit_0_59);
+        } catch (error) {
+            throw error;
+        }
+    }
 
     React.useEffect(() => {
         async function getUserInformation() {
@@ -14,6 +52,7 @@ const User = () => {
                 setUsername(response.data.username);
                 setEmail(response.data.email);
                 setAuth(true);
+                getStatsInformation()
             } catch (error) {
                 setAuth(false);
                 throw error;
@@ -33,7 +72,23 @@ const User = () => {
                     <UserInformation username={username} email={email} />
                 </div>
                 <div id="StatInformation-layout">
-                    <StatInformation />
+                    <StatInformation
+                        threeDartAvg={threeDartAvg}
+                        oneDartAvg={oneDartAvg}
+                        wins={wins}
+                        loses={loses}
+                        totalGames={totalGames}
+                        highestFinish={highestFinish}
+                        doublesHit={doublesHit}
+                        hit180={hit180}
+                        hit160={hit160}
+                        hit140={hit140}
+                        hit120={hit120}
+                        hit100={hit100}
+                        hit80={hit80}
+                        hit60={hit60}
+                        hit0={hit0}
+                    />
                 </div>
 
             </div>
