4dd2d04443d2b383f844ee7db6ba7abf66db0f66
mfy <Faisaly1234@outlook.com>
2021-05-18 23:44:45 +0100

    updated game component with websocket connection and event listners

--- client/src/pages/Game/index.js
+++ client/src/pages/Game/index.js
@@ -1,6 +1,39 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import { CurrentScore, Legdisplay, OpponentScore } from '../../components'
+
 const Game = () => {
+
+    useEffect(()=>{
+        const chatSocket = new WebSocket('ws://' + '0.0.0.0:8000' +'/ws/chat/'  + 'asd' +'/')
+        
+        chatSocket.onopen = function() {
+            console.log('workingggg')
+          }
+        
+        chatSocket.onmessage = function(e) {
+            const data = JSON.parse(e.data)
+            console.log(data.success)
+
+            switch (true) {
+                case data.success !== null :
+                    console.log('success woooo');
+                    break;
+                case data.message !== null :
+                    console.log('message recieved');
+                    break;
+                case data.score !== null:
+                    console.log('score recieved!');
+                    break;
+                default:
+                    console.log(`Sorry, server response doesn't match cases`);
+              }
+        }   
+        
+        chatSocket.onclose = function(e) {
+            console.error('Chat socket closed ');
+        }
+
+    })
 
     return (
         <>
