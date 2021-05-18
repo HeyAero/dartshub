630006b8a9613d20c7e19bf6f29c666b2ac65b11
HeyAero <aeroyts@icloud.com>
2021-05-18 13:55:35 +0100

    add reciever in stats views to add stats entry when new user is created

--- server/api/stats/views.py
+++ server/api/stats/views.py
@@ -1,52 +1,17 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-from django.core.serializers import serialize
+from rest_framework import status, permissions
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
-# # Create your views here.
-# from django.core.serializers import serialize
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
 
-# from django.http.response import JsonResponse
-# from rest_framework.parsers import JSONParser 
-# from rest_framework import status
+from .models import Stats
+from .serializers import StatsSerializers
 
-# from .models import Stats
-# from .serializers import StatsSerializers
-# from rest_framework.decorators import api_view
-# from django.contrib.auth.models import User
-
-# from rest_framework.permissions import IsAuthenticated
-
-# # Create your views here.
-# @api_view(['GET'])
-# def index(request):
-#   if request.method == 'GET':
-#     stats = Stats.objects.all()
-#     stats_serializer = StatsSerializers(stats, many=True)
-#     return JsonResponse(stats_serializer.data, safe=False)
-
-# @api_view(['GET', 'POST', 'PATCH'])
-# def individual_stats(request, id):
-#   # stats = Stats.objects.get(pk=id)
-#   username = request.POST['username']
-#   password = request.POST['password']
-#   if user is not None:
-#     print(user)
-#   else:
-#     print(user)
-#   # if stats.is_valid():
-#   #   if request.method == 'GET':
-#   #     return False
-#   #   elif request.method == 'POST':
-#   #     stats_serializer = StatsSerializers(data=user)
-#   #     if stats_serializer.is_valid():
-#   #       stats_serializer.save()
-#   #       return JsonResponse(stats_serializer.data, status=status.HTTP_201_CREATED)
-#   #     return JsonResponse(stats_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-#   #   elif request.method == 'PATCH':
-#   #     return False
-#   # else:
-#   #   stats_serializer = StatsSerializers(data=user)
-#   #   if stats_serializer.is_valid():
-#   #     stats_serializer.save()
-#   #     return JsonResponse(stats_serializer.data, status=status.HTTP_201_CREATED)
-#   #   return JsonResponse(stats_serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_stats_to_user(sender, instance=None, created=False, **kwargs):
+  if created:
+    print('Creating user stats')
+    new_stats = Stats.objects.create(user=instance)
+    new_stats.save()
