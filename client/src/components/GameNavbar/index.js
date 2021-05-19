941bf60c5d9cafd483328bea130f83c673c214bd
Emmanuel Sobamowo <sobamowo.laolu@hotmail.com>
2021-05-19 11:40:47 +0100

    Add some edittinf to logout modal

--- client/src/components/GameNavbar/index.js
+++ client/src/components/GameNavbar/index.js
@@ -2,27 +2,27 @@ import React from 'react';
 import Modal from 'react-modal';
 
 const GameNavbar = () => {
-    const [openLogout, setOpenLogout] = React.useState(false); 
+    const [openLogout, setOpenLogout] = React.useState(false);
     function openModalLogout() {
         setOpenLogout(true)
     }
     function closeModalLogout() {
         setOpenLogout(false)
     }
-  
+
     return (
         <>
             <header >
                 <div className="nav-container">
                     <nav className="navbar">
 
-                        <div  id="GameNav">
+                        <div id="GameNav">
 
 
-                            <a href='user'>Home</a>
+                            <a href='/'>Home</a>
 
 
-                            <button className="log" id="logout" onClick={openModalLogout}>Leave Game</button>
+                            <button className="log" id="logout" onClick={openModalLogout} >Leave Game</button>
 
 
                         </div>
@@ -32,10 +32,13 @@ const GameNavbar = () => {
             <Modal isOpen={openLogout}
                 onRequestClose={closeModalLogout}>
                 <div className="confirm">
-                    <form id="logout" method="post">
+                    <form id="logout" >
                         <h4>Leave Game</h4>
                         <p>Are you sure you want to leave the game?</p>
+
                         <input type="submit" id="logout-btn" value="Logout" />
+
+
                         <button onClick={closeModalLogout}>Cancel</button>
                     </form>
                 </div>
