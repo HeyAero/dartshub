2cdb30caa584692992299938cd9d16267a71c7ce
HeyAero <aeroyts@icloud.com>
2021-05-19 21:47:36 +0100

    potential fix for api.wsgi not being found

--- client/src/actions/getAuthInstance/index.js
+++ client/src/actions/getAuthInstance/index.js
@@ -7,7 +7,8 @@ const getAuthInstance = axios.create({
     headers: {
         'Authorization': "JWT " + localStorage.getItem('access_token'),
         'Content-Type': 'application/json',
-        'accept': 'application/json'
+        'accept': 'application/json',
+        'Access-Control-Allow-Origin': '*',
     }
 });
 
--- server/Procfile
+++ server/Procfile
@@ -1,2 +1,2 @@
-web: gunicorn api.wsgi
+web: gunicorn api/api.wsgi
 release: python api/manage.py migrate
