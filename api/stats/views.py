5b36c8681300c3b54cd568fa842a5435571acb6b
HeyAero <aeroyts@icloud.com>
2021-05-19 17:17:35 +0100

    update Game screen to take data and send it to api to update user after game is completed

--- api/stats/views.py
+++ api/stats/views.py
@@ -27,31 +27,42 @@ class StatsFetch(APIView):
     def put(self, request):
       user = request.user
       if user:
+        incoming_data = request.data['stats']
+        print("INCOMING DATA")
+        print(incoming_data)
         stats = Stats.objects.get(user=user)
         old_data = StatsSerializers(stats)
         old_data = old_data.data
         if old_data:
           old_total = old_data['total_games']
-          tda = ((float(old_data['three_dart_avg']) * old_total) + (float(request.data['three_dart_avg']))) / (old_data['total_games'] + 1)
-          oda = ((float(old_data['one_dart_avg']) * old_total) + (float(request.data['one_dart_avg']))) / (old_data['total_games'] + 1)
+          tda = ((float(old_data['three_dart_avg']) * old_total) + (float(incoming_data['three_dart_avg']))) / (old_data['total_games'] + 1)
+          tda = "{:.2f}".format(tda)
+          oda = ((float(old_data['one_dart_avg']) * old_total) + (float(incoming_data['one_dart_avg']))) / (old_data['total_games'] + 1)
+          oda = "{:.2f}".format(oda)
 
           old_data['total_games'] = old_data['total_games'] + 1
           old_data['three_dart_avg'] = tda
           old_data['one_dart_avg'] = oda
-          old_data['wins'] = old_data['wins'] + request.data['wins']
-          old_data['loses'] = old_data['loses'] + request.data['loses']
-          if old_data['highest_finish'] < request.data['highest_finish']:
-            old_data['highest_finish'] = request.data['highest_finish']
-          old_data['doubles_hit'] = old_data['doubles_hit'] + request.data['doubles_hit']
-          old_data['hit_180'] = old_data['hit_180'] + request.data['hit_180']
-          old_data['hit_160_179'] = old_data['hit_160_179'] + request.data['hit_160_179']
-          old_data['hit_140_159'] = old_data['hit_140_159'] + request.data['hit_140_159']
-          old_data['hit_120_139'] = old_data['hit_120_139'] + request.data['hit_120_139']
-          old_data['hit_100_119'] = old_data['hit_100_119'] + request.data['hit_100_119']
-          old_data['hit_80_99'] = old_data['hit_80_99'] + request.data['hit_80_99']
-          old_data['hit_60_79'] = old_data['hit_60_79'] + request.data['hit_60_79']
-          old_data['hit_0_59'] = old_data['hit_0_59'] + request.data['hit_0_59']
+          old_data['wins'] = old_data['wins'] + incoming_data['wins']
+          old_data['loses'] = old_data['loses'] + incoming_data['loses']
+          if old_data['highest_finish'] < incoming_data['highest_finish']:
+            old_data['highest_finish'] = incoming_data['highest_finish']
+          old_data['doubles_hit'] = old_data['doubles_hit'] + incoming_data['doubles_hit']
+          old_data['hit_180'] = old_data['hit_180'] + incoming_data['hit_180']
+          old_data['hit_160_179'] = old_data['hit_160_179'] + incoming_data['hit_160_179']
+          old_data['hit_140_159'] = old_data['hit_140_159'] + incoming_data['hit_140_159']
+          old_data['hit_120_139'] = old_data['hit_120_139'] + incoming_data['hit_120_139']
+          old_data['hit_100_119'] = old_data['hit_100_119'] + incoming_data['hit_100_119']
+          old_data['hit_80_99'] = old_data['hit_80_99'] + incoming_data['hit_80_99']
+          old_data['hit_60_79'] = old_data['hit_60_79'] + incoming_data['hit_60_79']
+          old_data['hit_0_59'] = old_data['hit_0_59'] + incoming_data['hit_0_59']
 
           serializer = StatsSerializers(stats, data=old_data)
+          print("serializer")
+          print(serializer)
+          print("IS VALID?")
+          print(serializer.is_valid())
           if serializer.is_valid():
+            print("TEST")
             serializer.save()
+            return Response(serializer.data, status=status.HTTP_200_OK)
