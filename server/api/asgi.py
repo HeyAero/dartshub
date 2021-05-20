7a6349ae6779bc2d165d71832872b2193e8a1082
HeyAero <aeroyts@icloud.com>
2021-05-20 10:58:20 +0100

    add django setup to asgi file

--- server/api/asgi.py
+++ server/api/asgi.py
@@ -8,13 +8,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 """
 
 import os
-
+import django
 from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 import gameroom.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
+
+django.setup()
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
