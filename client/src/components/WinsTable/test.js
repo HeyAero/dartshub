60d31b72034defa4ad68a287ce08c307186daafe
mfy <Faisaly1234@outlook.com>
2021-05-20 13:58:02 +0100

    added tests for windtable

---
+++ client/src/components/WinsTable/test.js
@@ -0,0 +1,17 @@
+import { prettyDOM, screen } from '@testing-library/react';
+import WinsTable from '.';
+
+
+describe('Winstable', () => {
+    beforeEach(() => {
+        render(<WinsTable/>);
+    })
+
+    test('it renders with a table on successfully', async () => {
+        const table = await screen.findByRole('table');
+        expect(table).toBeInTheDocument()
+        expect(screen.getByText('USERNAME')).toBeInTheDocument();
+        expect(screen.getByText('NUMBER OF WINS')).toBeInTheDocument();
+
+    })
+})
