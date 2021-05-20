080dbe53959a64c8f873844a9fe6b64bd35e38e1
HeyAero <aeroyts@icloud.com>
2021-05-20 11:13:34 +0100

    move imports down on asgi to fix potential issues

--- api/asgi.py
+++ api/asgi.py
@@ -9,14 +9,15 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 import django
-from channels.auth import AuthMiddlewareStack
 from django.core.asgi import get_asgi_application
-from channels.routing import ProtocolTypeRouter, URLRouter
-import gameroom.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 django.setup()
 django_asgi_app = get_asgi_application()
+
+from channels.auth import AuthMiddlewareStack
+from channels.routing import ProtocolTypeRouter, URLRouter
+import gameroom.routing
 
 application = ProtocolTypeRouter({
     "http": django_asgi_app,
