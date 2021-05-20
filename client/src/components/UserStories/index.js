6f5dad07efc5a12e0c4801c09a8ccf9403d85007
HeyAero <aeroyts@icloud.com>
2021-05-20 18:00:51 +0100

    update user stories styling

--- client/src/components/UserStories/index.js
+++ client/src/components/UserStories/index.js
@@ -3,13 +3,25 @@ import React from 'react';
 const UserStories = () => {
 
     return (
-        <>
-        
-            <h1>User Stories!</h1>
-           
-        
-
-        </>
+        <div id="user-stories">
+            <div className="container">
+                <h1>User Stories!</h1>
+                <div className="row">
+                    <div className="col user-story">
+                        <h5>"A great app that allows me to play with my friends"</h5>
+                        <p>- John</p>
+                    </div>
+                    <div className="col user-story">
+                        <h5>"Finally an app which tracks stats!!!"</h5>
+                        <p>- Aaron</p>
+                    </div>
+                    <div className="col user-story">
+                        <h5>"Great for a free app, would recommend to any player"</h5>
+                        <p>- Faisal</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
 export default UserStories;
