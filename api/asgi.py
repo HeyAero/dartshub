ca21a7967d3e4f45d4e32014ab999df4447c328a
HeyAero <aeroyts@icloud.com>
2021-05-20 10:41:17 +0100

    update settings on django to fix redis issues

--- api/asgi.py
+++ api/asgi.py
@@ -14,7 +14,7 @@ from django.core.asgi import get_asgi_application
 from channels.routing import ProtocolTypeRouter, URLRouter
 import gameroom.routing
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'core.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'api.settings')
 
 application = ProtocolTypeRouter({
     "http": get_asgi_application(),
